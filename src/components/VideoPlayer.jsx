d25a963876dabe6b83c574958adf20031e5ee687
winniefong1021 <winniefong21@gmail.com>
2019-04-22 16:50:08 -0700

    Refactor App to be a class

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,13 @@
-var VideoPlayer = () => (
+var VideoPlayer = (props) => (
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video[0].id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video[0].snippet.title}</h3>
+      <div>{props.video[0].snippet.description}</div>
     </div>
-  </div>
+  </div >
 );
 
 // PropTypes tell other developers what `props` a component expects
